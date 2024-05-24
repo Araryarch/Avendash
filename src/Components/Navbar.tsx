4a3eb788642701b0aed7a7b0b65f2d549a8b2f4d
Araryarch <Araryaarch@gmail.com>
2024-05-25 03:05:18 +0700

    penyesuaian tampilan

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -7,7 +7,7 @@ interface NavbarProps {
 const Navbar = ({ addClass }: NavbarProps) => {
   return (
     <nav className={`${addClass} navbar-container navbar mx-auto my-5`}>
-      <div className="navbar rounded-box bg-base-100 shadow-sm shadow-primary">
+      <div className="shadow-sm navbar rounded-box bg-base-100 shadow-primary">
         <div className="navbar-start">
           <div className="dropdown">
             <div
@@ -17,7 +17,7 @@ const Navbar = ({ addClass }: NavbarProps) => {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
+                className="w-5 h-5"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -40,35 +40,151 @@ const Navbar = ({ addClass }: NavbarProps) => {
               <li>
                 <NavLink to={'/sensors'}>Dashboard</NavLink>
               </li>
-              <li>
-                <select
-                  className="select select-bordered my-2 w-full max-w-xs"
-                  onChange={(e) => {
-                    document.documentElement.setAttribute(
-                      'data-theme',
-                      e.target.value,
-                    )
-                  }}
-                >
-                  <option disabled selected>
-                    Theme
-                  </option>
-                  <option value="retro">Retro</option>
-                  <option value="cyberpunk">Cyberpunk</option>
-                  <option value="valentine">Valentine</option>
-                  <option value="coffee">Coffee</option>
-                </select>
-              </li>
             </ul>
           </div>
         </div>
         <div className="navbar-center">
-          <NavLink to={'/'} className="btn btn-ghost text-xl">
+          <NavLink to={'/'} className="text-xl btn btn-ghost">
             Avendash
           </NavLink>
         </div>
         <div className="navbar-end">
-          <img src="img/logo.png" alt="" width={50} />
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="m-1 btn">
+              Theme
+              <svg
+                width="12px"
+                height="12px"
+                className="inline-block w-2 h-2 fill-current opacity-60"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 2048 2048"
+              >
+                <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] w-52 rounded-box bg-base-300 p-2 shadow-2xl"
+            >
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="Default"
+                  value="default"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="Retro"
+                  value="retro"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="Cyberpunk"
+                  value="cyberpunk"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="Valentine"
+                  value="valentine"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="wireframe"
+                  value="wireframe"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="autumn"
+                  value="autumn"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="black"
+                  value="black"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="luxury"
+                  value="luxury"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="coffee"
+                  value="coffee"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="nord"
+                  value="nord"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="pastel"
+                  value="pastel"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="cupcake"
+                  value="cupcake"
+                />
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="theme-dropdown"
+                  className="justify-start theme-controller btn btn-ghost btn-sm btn-block"
+                  aria-label="halloween"
+                  value="halloween"
+                />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
