1a14f41ade7681e77c99601af499ba8def9ef852
Araryarch <Araryaarch@gmail.com>
2024-05-24 23:45:56 +0700

    :v

--- src/Components/Sensor.tsx
+++ src/Components/Sensor.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import AI from './AI'
 import StatSensor from './Sensors/StatSensor'
 
@@ -9,35 +9,43 @@ interface SensorProps {
   sensor4?: string | number
 }
 
+const calculateBrightness = (ldrRawValue: number) => {
+  const gama = 0.7
+  const rl10 = 50
+
+  ldrRawValue = map(ldrRawValue, 4095, 0, 1024, 0)
+
+  const voltage = (ldrRawValue / 1024.0) * 5
+  const resistance = (2000 * voltage) / (1 - voltage / 5)
+  const brightness = Math.pow(
+    (rl10 * 1e3 * Math.pow(10, gama)) / resistance,
+    1 / gama,
+  )
+
+  return brightness
+}
+
+const map = (
+  x: number,
+  in_min: number,
+  in_max: number,
+  out_min: number,
+  out_max: number,
+): number => {
+  return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
+
 const Sensor = ({ sensor1, sensor2, sensor3, sensor4 }: SensorProps) => {
-  const calculateBrightness = (ldrRawValue: number) => {
-    const gama = 0.7
-    const rl10 = 50
+  const [brightness, setBrightness] = useState<number | undefined>(undefined)
 
-    ldrRawValue = map(ldrRawValue, 4095, 0, 1024, 0)
-
-    const voltage = (ldrRawValue / 1024.0) * 5
-    const resistance = (2000 * voltage) / (1 - voltage / 5)
-    const brightness = Math.pow(
-      (rl10 * 1e3 * Math.pow(10, gama)) / resistance,
-      1 / gama,
-    )
-
-    return brightness
-  }
-
-  const map = (
-    x: number,
-    in_min: number,
-    in_max: number,
-    out_min: number,
-    out_max: number,
-  ): number => {
-    return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-  }
-
-  const brightness = Math.ceil(calculateBrightness(Number(sensor3) || 0))
-  console.log(brightness)
+  useEffect(() => {
+    if (sensor3 !== undefined) {
+      const brightnessValue = calculateBrightness(Number(sensor3) || 0)
+      setBrightness(Math.ceil(brightnessValue))
+    } else {
+      setBrightness(undefined)
+    }
+  }, [sensor3])
 
   return (
     <div className="grid items-center justify-center w-full min-h-screen grid-cols-1 gap-5 px-12 sensors xl:-mt-16 xl:grid-cols-2 xl:gap-5">
